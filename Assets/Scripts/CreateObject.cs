681d64dcc1ab42d152f11e9b6e69e4ab0f4a8de7
Luciano Reul <lucianoreul@gmail.com>
2018-10-04 22:13:03 -0300

    Complete the exercise commit

--- Assets/Scripts/CreateObject.cs
+++ Assets/Scripts/CreateObject.cs
@@ -3,33 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using LitJson;
 
 public class CreateObject : MonoBehaviour
 {
     public GameObject gameObjectPrefab;
     public Button createBtn;
-    public TMP_InputField positionX;
-    public TMP_InputField positionY;
-    public TMP_InputField positionZ;
-    public TMP_InputField rotationX;
-    public TMP_InputField rotationY;
-    public TMP_InputField rotationZ;
+    //public TMP_InputField positionX;
+    //public TMP_InputField positionY;
+    //public TMP_InputField positionZ;
+    //public TMP_InputField rotationX;
+    //public TMP_InputField rotationY;
+    //public TMP_InputField rotationZ;
 
     private void Start()
     {
-        createBtn.onClick.AddListener(delegate
+        //createBtn.onClick.AddListener(delegate
+        //{
+        //    var postitotion = new Vector3(float.Parse(positionX.text), float.Parse(positionY.text), float.Parse(positionZ.text));
+        //    Quaternion rotation = Quaternion.Euler(float.Parse(rotationX.text), float.Parse(rotationY.text), float.Parse(rotationZ.text));
+        //    CreateGameObjectFromPositionAndRotation(
+        //        gameObjectPrefab,
+        //        postitotion,
+        //        rotation);
+        //});
+
+        createBtn.onClick.AddListener(delegate 
         {
-            var postitotion = new Vector3(float.Parse(positionX.text), float.Parse(positionY.text), float.Parse(positionZ.text));
-            Quaternion rotation = Quaternion.Euler(float.Parse(rotationX.text), float.Parse(rotationY.text), float.Parse(rotationZ.text));
-            CreateGameObjectFromPositionAndRotation(
-                gameObjectPrefab,
-                postitotion,
-                rotation);
+            StartCoroutine(GetPositionAndRotation());
         });
     }
 
     private void CreateGameObjectFromPositionAndRotation(GameObject prefab, Vector3 position, Quaternion rotation)
     {
-        var newObject = Instantiate(prefab, position, rotation);
+        Instantiate(prefab, position, rotation);
+    }
+
+    private IEnumerator GetPositionAndRotation()
+    {
+        string url = "https://raw.githubusercontent.com/lucianoreul/Unity-AR-Developer-Vuforia/master/Assets/PositionAndRotation.txt";
+        WWW w = new WWW(url);
+        yield return w;
+        if (w.error != null)
+        {
+            Debug.Log(w.error);
+        }
+        else
+        {
+            JsonData data = JsonMapper.ToObject(w.text);
+            var position = new Vector3(float.Parse(data["position"]["x"].ToString()),
+                                       float.Parse(data["position"]["y"].ToString()),
+                                       float.Parse(data["position"]["z"].ToString()));
+            var rotation = Quaternion.Euler(float.Parse(data["rotation"]["x"].ToString()),
+                                            float.Parse(data["rotation"]["y"].ToString()),
+                                            float.Parse(data["rotation"]["z"].ToString()));
+            gameObjectPrefab.transform.localPosition = position;
+            gameObjectPrefab.transform.localRotation = rotation;
+            Debug.Log(position);
+        }
     }
 }
